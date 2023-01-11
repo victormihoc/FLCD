3af096481d9d3dd3109706bfc9bf5e849697ea48
Victor <72305425+victormihoc@users.noreply.github.com>
2023-01-11 21:50:47 +0200

    Delete lab5/parsing directory

--- lab5/parsing/main.py
+++
@@ -1,8 +0,0 @@
-from model.grammar import Grammar
-from model.parser import Parser
-
-if __name__ == '__main__':
-    gramm = Grammar()
-    # gramm.run()
-    parser = Parser(gramm)
-    parser.recursive_descent('acbc')
